203ad171be15db25f9c10f7f6569842558e49615
354873 <110684627+354873@users.noreply.github.com>
2022-09-15 16:49:09 +0900

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,10 @@
-alert("遊ぶかー")
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LX4BH3ZLY0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LX4BH3ZLY0');
+</script>
